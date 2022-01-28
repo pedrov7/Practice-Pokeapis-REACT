b18da1c462b0e42f132158b7535b7c7ca40288a3
pedrov7 <pedro7valla@hotmail.com>
2022-01-27 22:23:20 -0500

    Arreglo de boton

--- src/components/BringPokemon.jsx
+++ src/components/BringPokemon.jsx
@@ -4,7 +4,9 @@ import React, { useEffect, useState } from 'react';
 export const BringPokemon = () => {
 
     const [writePoke, setWritePoke] = useState([]);
-    const [showPoke, setShowPoke] = useState();
+    // pokemonList, setPokemonList
+    const [showPoke, setShowPoke] = useState(false);
+    // hacerlo booleano
 
     useEffect(() => {
         // fetch('https://pokeapi.co/api/v2/pokemon')
@@ -14,13 +16,7 @@ export const BringPokemon = () => {
     }, []);
 
     const handleOnClick = () => {
-            setShowPoke(
-            <ol>
-                {writePoke.length > 0 && writePoke.map((pokeName, index) => {
-                    return (<li key = { index }>{pokeName.name}</li>)}
-                )}
-            </ol>
-            )
+            setShowPoke(!showPoke)
     }
 
     return (
@@ -28,7 +24,16 @@ export const BringPokemon = () => {
         <div style={{ width: 500 }}>
 
             <button onClick={handleOnClick} type="button" className="btn btn-secondary">Fetch Pokemon</button>
-            {showPoke}
+            {/* {showPoke} */}
+
+
+            
+            {showPoke && <ol>
+                {writePoke.length > 0 && writePoke.map((pokeName, index) => {
+                    return (<li key = { index }>{pokeName.name}</li>)}
+                )}
+            </ol>
+            }
 
         </div>);
 };
